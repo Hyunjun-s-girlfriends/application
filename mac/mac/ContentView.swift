635f5d3ede2539635a91ed26389ab7d23480f1b3
gbswm0929 <gbswst0929@gmail.com>
2026-05-06 16:16:48 +0900

    .

---
+++ mac/mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac
+//
+//  Created by gbsw on 5/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac/mac/macApp.swift
@@ -0,0 +1,17 @@
+//
+//  macApp.swift
+//  mac
+//
+//  Created by gbsw on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
